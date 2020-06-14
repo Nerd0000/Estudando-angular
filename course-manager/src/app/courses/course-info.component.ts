6badec977df6bf306e9706ce413440e7862788ab
Alan C <alancsystem@gmail.com>
2020-06-14 21:36:10 +0000

    snap

--- course-manager/src/app/courses/course-info.component.ts
+++ course-manager/src/app/courses/course-info.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Course } from './course';
-import { CourseService } from './course.service';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Course } from './course'
+import { CourseService } from './course.service'
 
 @Component({
   templateUrl: './course-info.component.html'
@@ -10,21 +10,29 @@ import { CourseService } from './course.service';
 export class CourseInfoComponent implements OnInit {
 
   // Recebe os cursos
-  course: Course;
+  course: Course
 
   // Consome as rotas e o courseService
   constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
 
   // Retorna as informacoes de um curso
   ngOnInit(): void {
-    this.course = this.courseService
-      .retrieveById(+this.activatedRoute.snapshot.paramMap
-        .get('id'))
+    this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
+      // Em caso de sucesso
+      next: course => this.course = course,
+      // Em caso de erro
+      error: err => console.log("Error: ", err)
+    })
   }
 
   // Altera as informacoes de um curso
   save(): void {
-    this.courseService.save(this.course);
+    this.courseService.save(this.course).subscribe({
+      // Em caso de sucesso
+      next: course => console.log("Saved with success", course),
+      // Em caso de erro
+      error: err => console.log('Error: ', err)
+    })
   }
 
 }
--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -8,7 +8,7 @@ import { CourseService } from './course.service';
 
 export class CourseListComponent implements OnInit {
 
-  _courses: Course[] = [];
+  _courses: Course[] = []
   _filterBy: string
   filteredCourses: Course[] = []
 
@@ -17,20 +17,45 @@ export class CourseListComponent implements OnInit {
     this.filteredCourses = this._courses
       .filter((course: Course) => course.name
         .toLocaleLowerCase()
-        .indexOf(this._filterBy
-          .toLocaleLowerCase()) > -1)
+          .indexOf(this._filterBy
+            .toLocaleLowerCase()) > -1)
   }
   get filter() {
     return this._filterBy
   }
 
-  // Puxa o CourseService de course.service.ts
+  // Importa o CourseService de course.service.ts
   constructor(private courseService: CourseService) { }
 
   // Executa a funcao que retorna todos os cursos
   ngOnInit(): void {
-    this._courses = this.courseService.retrieveAll()
-    this.filteredCourses = this._courses
+    this.retrieveAll()
+  }
+
+  // Faz a busca pelos cursos na api
+  retrieveAll(): void {
+    this.courseService.retrieveAll().subscribe({
+      // Em caso de sucesso
+      next: courses => {
+        this._courses = courses
+        this.filteredCourses = this._courses
+      },
+      // Em caso de erro
+      error: err => console.log("Error: ", err)
+    })
+  }
+
+  // Deleta o curso com id informado
+  deleteById(courseId: number): void {
+    this.courseService.deleteById(courseId).subscribe({
+      // Em caso de sucesso
+      next: () => {
+        console.log("Deleted with success")
+        this.retrieveAll()
+      },
+      // Em caso de erro
+      error: err => console.log("Error: ", err)
+    })
   }
 
 }
